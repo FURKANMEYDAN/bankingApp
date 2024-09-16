03e45a03edcbe0af40961f14feb465fd0f662363
FURKANMEYDAN <bfmeydan@gmail.com>
2024-09-16 22:18:51 +0300

    1.saat

--- banking-app/app/(root)/page.tsx
+++ banking-app/app/(root)/page.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 const Home = () => {
+    const loggedIn = {firstName : 'Furkan'};
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName||'Guest'}
+                    subtext="Access and managae your account and transactions effiecently."
+                />
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
