d75c32ff171f1664278234339d945d68ed4cdf79
FURKANMEYDAN <bfmeydan@gmail.com>
2024-09-18 23:00:53 +0300

    Implement the complete layout

--- banking-app/app/(root)/page.tsx
+++ banking-app/app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar';
 const Home = () => {
-    const loggedIn = {firstName : 'Furkan'};
+    const loggedIn = {firstName : 'Furkan',lastName:'jsm',email:'contact@furkan.me'};
   return (
     <section className='home'>
         <div className='home-content'>
@@ -19,7 +20,13 @@ const Home = () => {
                     totalCurrentBalance={1250.35}
                 />
             </header>
+            Recent transactions
         </div>
+        <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance : 123.50},{currentBalance:500.50}]}
+        />
     </section>
   )
 }
